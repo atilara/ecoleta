c096a95e102dfc5c899184604aa28f1498d9da26
Átila Rodrigues <atila.rodriguesal@gmail.com>
2020-06-03 12:11:14 -0300

    Added routes, constructed the homepage and form structure

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,23 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
-import Header from './Header';
+import Routes from './routes';
 
 function App() {
-	// a função useState retorna o valor inicial e uma função utilizada para atualizar o valor do counter
-	// conceito de imutalibilidade não permite que o componente seja alterado diretamente
-	const [counter, setCounter] = useState(0);
-
-	function handleButtonClick() {
-		setCounter(counter + 1);
-	}
-	
 	return (
-		<div>
-			<Header title={`Counter: ${counter}`}/>
-			<h1>{counter}</h1>
-			<button type="button" onClick={handleButtonClick}>Sum</button>
-		</div>
+		<Routes />
 	);
 }
 
