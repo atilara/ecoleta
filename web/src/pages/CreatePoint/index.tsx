ae2a8856e27904f5f64ecf20886fa603668b4e42
Átila Rodrigues <atila.rodriguesal@gmail.com>
2020-06-03 14:22:17 -0300

    Integrated the website with the api, it can now search for the items

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
+import api from '../../services/api';
 
 import './styles.css';
 
 import logo from '../../assets/logo.svg';
 
+interface Item {
+    id: number;
+    title: string;
+    image_url: string;
+}
+
 const CreatePoint = () => {
+    // criação de um estado, com o paramêtro <Item[]> o typescript entende que
+    // todos os dados desse array terão as propriedades especificadas na interface  
+    const [items, setItems] = useState<Item[]>([]);
+    
+    useEffect(() => {
+        api.get('items').then(response => {
+            setItems(response.data);
+        });
+    }, []);
+    
     return(
         <div id="page-create-point">
             <header>
@@ -85,31 +102,14 @@ const CreatePoint = () => {
                         <h2>Ítens de coleta</h2>
                         <span>Selecione um ou mais itens abaixo</span>
                     </legend>
+                    
                     <ul className="items-grid">
-                        <li>
-                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="lampadas"/>
-                            <span>Lâmpadas</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="lampadas"/>
-                            <span>Lâmpadas</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="lampadas"/>
-                            <span>Lâmpadas</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="lampadas"/>
-                            <span>Lâmpadas</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="lampadas"/>
-                            <span>Lâmpadas</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="lampadas"/>
-                            <span>Lâmpadas</span>
-                        </li>
+                        {items.map(item => (
+                            <li key={item.id}>
+                                <img src={item.image_url} alt={item.title} />
+                                <span>{item.title}</span>
+                            </li>
+                        ))}
                     </ul>
                 </fieldset>
                 <button type="submit">
