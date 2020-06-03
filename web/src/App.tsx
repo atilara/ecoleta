333f203e29f8117ce306f32f726c0d4c7c6f75f5
Átila Rodrigues <atila.rodriguesal@gmail.com>
2020-06-03 10:33:44 -0300

    Removing unnecessary files and understading props

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import Header from './Header';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world</h1>
-      </header>
-    </div>
-  );
+	return (
+		<Header title="Title from props"/>
+	);
 }
 
 export default App;
