68d4b8de64f50d5d6fdcb7699e93e35a85593eb5
Átila Rodrigues <atila.rodriguesal@gmail.com>
2020-06-03 10:57:40 -0300

    Understanding props and state

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import Header from './Header';
 
 function App() {
+	// a função useState retorna o valor inicial e uma função utilizada para atualizar o valor do counter
+	// conceito de imutalibilidade não permite que o componente seja alterado diretamente
+	const [counter, setCounter] = useState(0);
+
+	function handleButtonClick() {
+		setCounter(counter + 1);
+	}
+	
 	return (
-		<Header title="Title from props"/>
+		<div>
+			<Header title={`Counter: ${counter}`}/>
+			<h1>{counter}</h1>
+			<button type="button" onClick={handleButtonClick}>Sum</button>
+		</div>
 	);
 }
 
--- web/src/Header.tsx
+++ web/src/Header.tsx
@@ -4,6 +4,9 @@ interface HeaderProps {
     title: string;
 }
 
+// React.FunctionComponent, ou seja, componente em forma de função
+// Transforma-se o componente em constante e se usa ArrowFunction
+// A interface indica quais são as propriedades que o componente pode receber (Lembra um contrato, interface em java)
 const Header: React.FC<HeaderProps> = (props) => {
     return (
         <h1>{props.title}</h1>
