6b8493147052c54d251fd27b96ce3d6d5a7b140b
Átila Rodrigues <atila.rodriguesal@gmail.com>
2020-06-05 16:23:10 -0300

    Created the Map page

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -1,105 +1,204 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import { Feather as Icon } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
+import { View, StyleSheet, Image, TouchableOpacity, Text, ScrollView } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { SvgUri } from 'react-native-svg';
 
 const Points = () => {
-  return <View />
-} 
+
+	// "instância"
+	const navigation = useNavigation();
+
+	// faz o usuário voltar a tela anterior quando o botão é clidado
+	function handleNavigateBack() {
+		navigation.goBack();
+	}
+
+	function handleNavigateToDetail() {
+		navigation.navigate('Detail');
+	}
+	
+	// as duas chaves em alguns componentes {{}} representam que aquilo é um código js e objeto
+	
+	return (
+		<>
+			<View style={styles.container}> 
+				<TouchableOpacity onPress={handleNavigateBack}>
+					<Icon name="arrow-left" size={20} color="#34cb79" />
+				</TouchableOpacity>
+				<Text style={styles.title}>Bem vindo!</Text>
+				<Text style={styles.description}>Encontre no mapa um ponto de coleta.</Text>
+
+				<View style={styles.mapContainer}>
+					<MapView 
+						style={styles.map} 
+						initialRegion={{
+							latitude: -8.0113981,
+							longitude: -34.8734516,
+							latitudeDelta: 0.014,
+							longitudeDelta: 0.014
+						}}
+					>
+						<Marker 
+							style={styles.mapMarker}
+							onPress={handleNavigateToDetail}
+							coordinate={{
+								latitude: -8.0113981,
+								longitude: -34.8734516
+							}} 
+						>
+							<View style={styles.mapMarkerContainer}>
+								<Image 
+									style={styles.mapMarkerImage} 
+									source={{ 
+										uri: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60'
+									}} 
+								/>
+								<Text style={styles.mapMarkerTitle}>Mercado</Text>
+							</View>
+						</Marker>
+					</MapView>
+				</View>
+			</View>
+			<View style={styles.itemsContainer}>
+				<ScrollView 
+					horizontal 
+					showsHorizontalScrollIndicator={false}
+					contentContainerStyle={{
+						paddingHorizontal: 20
+					}}
+				>
+					<TouchableOpacity style={styles.item} onPress={() => {}}>
+						<SvgUri 
+							width={42} 
+							height={42} 
+							uri="http://192.168.15.2:3333/uploads/lampadas.svg" 
+						/>
+						<Text style={styles.itemTitle}>Lâmpadas</Text>
+					</TouchableOpacity>
+					<TouchableOpacity style={styles.item} onPress={() => {}}>
+						<SvgUri width={42} height={42} uri="http://192.168.15.2:3333/uploads/lampadas.svg" />
+						<Text style={styles.itemTitle}>Lâmpadas</Text>
+					</TouchableOpacity>
+					<TouchableOpacity style={styles.item} onPress={() => {}}>
+						<SvgUri width={42} height={42} uri="http://192.168.15.2:3333/uploads/lampadas.svg" />
+						<Text style={styles.itemTitle}>Lâmpadas</Text>
+					</TouchableOpacity>
+					<TouchableOpacity style={styles.item} onPress={() => {}}>
+						<SvgUri width={42} height={42} uri="http://192.168.15.2:3333/uploads/lampadas.svg" />
+						<Text style={styles.itemTitle}>Lâmpadas</Text>
+					</TouchableOpacity>
+					<TouchableOpacity style={styles.item} onPress={() => {}}>
+						<SvgUri width={42} height={42} uri="http://192.168.15.2:3333/uploads/lampadas.svg" />
+						<Text style={styles.itemTitle}>Lâmpadas</Text>
+					</TouchableOpacity>
+					<TouchableOpacity style={styles.item} onPress={() => {}}>
+						<SvgUri width={42} height={42} uri="http://192.168.15.2:3333/uploads/lampadas.svg" />
+						<Text style={styles.itemTitle}>Lâmpadas</Text>
+					</TouchableOpacity>
+				</ScrollView>
+			</View>
+		</>
+	);
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingHorizontal: 32,
-    //paddingTop: 20 + Constants.statusBarHeight,
-  },
+	container: {
+		flex: 1,
+		paddingHorizontal: 32,
+		paddingTop: 20 + Constants.statusBarHeight,
+	},
 
-  title: {
-    fontSize: 20,
-    fontFamily: 'Ubuntu_700Bold',
-    marginTop: 24,
-  },
+	title: {
+		fontSize: 20,
+		fontFamily: 'Ubuntu_700Bold',
+		marginTop: 24,
+	},
 
-  description: {
-    color: '#6C6C80',
-    fontSize: 16,
-    marginTop: 4,
-    fontFamily: 'Roboto_400Regular',
-  },
+	description: {
+		color: '#6C6C80',
+		fontSize: 16,
+		marginTop: 4,
+		fontFamily: 'Roboto_400Regular',
+	},
 
-  mapContainer: {
-    flex: 1,
-    width: '100%',
-    borderRadius: 10,
-    overflow: 'hidden',
-    marginTop: 16,
-  },
+	mapContainer: {
+		flex: 1,
+		width: '100%',
+		borderRadius: 10,
+		overflow: 'hidden',
+		marginTop: 16,
+	},
 
-  map: {
-    width: '100%',
-    height: '100%',
-  },
+	map: {
+		width: '100%',
+		height: '100%',
+	},
 
-  mapMarker: {
-    width: 90,
-    height: 80, 
-  },
+	mapMarker: {
+		width: 90,
+		height: 80, 
+	},
 
-  mapMarkerContainer: {
-    width: 90,
-    height: 70,
-    backgroundColor: '#34CB79',
-    flexDirection: 'column',
-    borderRadius: 8,
-    overflow: 'hidden',
-    alignItems: 'center'
-  },
+	mapMarkerContainer: {
+		width: 90,
+		height: 70,
+		backgroundColor: '#34CB79',
+		flexDirection: 'column',
+		borderRadius: 8,
+		overflow: 'hidden',
+		alignItems: 'center'
+	},
 
-  mapMarkerImage: {
-    width: 90,
-    height: 45,
-    resizeMode: 'cover',
-  },
+	mapMarkerImage: {
+		width: 90,
+		height: 45,
+		resizeMode: 'cover',
+	},
 
-  mapMarkerTitle: {
-    flex: 1,
-    fontFamily: 'Roboto_400Regular',
-    color: '#FFF',
-    fontSize: 13,
-    lineHeight: 23,
-  },
+	mapMarkerTitle: {
+		flex: 1,
+		fontFamily: 'Roboto_400Regular',
+		color: '#FFF',
+		fontSize: 13,
+		lineHeight: 23,
+	},
 
-  itemsContainer: {
-    flexDirection: 'row',
-    marginTop: 16,
-    marginBottom: 32,
-  },
+	itemsContainer: {
+		flexDirection: 'row',
+		marginTop: 16,
+		marginBottom: 32,
+	},
 
-  item: {
-    backgroundColor: '#fff',
-    borderWidth: 2,
-    borderColor: '#eee',
-    height: 120,
-    width: 120,
-    borderRadius: 8,
-    paddingHorizontal: 16,
-    paddingTop: 20,
-    paddingBottom: 16,
-    marginRight: 8,
-    alignItems: 'center',
-    justifyContent: 'space-between',
+	item: {
+		backgroundColor: '#fff',
+		borderWidth: 2,
+		borderColor: '#eee',
+		height: 120,
+		width: 120,
+		borderRadius: 8,
+		paddingHorizontal: 16,
+		paddingTop: 20,
+		paddingBottom: 16,
+		marginRight: 8,
+		alignItems: 'center',
+		justifyContent: 'space-between',
 
-    textAlign: 'center',
-  },
+		textAlign: 'center',
+	},
 
-  selectedItem: {
-    borderColor: '#34CB79',
-    borderWidth: 2,
-  },
+	selectedItem: {
+		borderColor: '#34CB79',
+		borderWidth: 2,
+	},
 
-  itemTitle: {
-    fontFamily: 'Roboto_400Regular',
-    textAlign: 'center',
-    fontSize: 13,
-  },
+	itemTitle: {
+		fontFamily: 'Roboto_400Regular',
+		textAlign: 'center',
+		fontSize: 13,
+	},
 });
 
 export default Points;
